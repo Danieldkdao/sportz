5eca64ca53704e163aa41a0c0ae21a5a1c320ba7
Daniel Dao <danieldkdao@gmail.com>
2026-03-26 20:06:06 -0500

    initialized express server

---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import express, { type Request, type Response } from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (_: Request, res: Response) => {
+  res.send("Welcome to the Sportz Server!");
+});
+
+app.listen(8000, () => console.log("Express Server listening on PORT 8000!"));
