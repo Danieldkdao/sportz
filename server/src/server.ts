55a26a94737bb0eb7e7673bcc0616a92851583b1
Daniel Dao <danieldkdao@gmail.com>
2026-03-27 00:39:07 -0500

    implemented WebSocket server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,28 @@
-import express, { type Request, type Response } from "express";
 import dotenv from "dotenv";
+import express, { type Request, type Response } from "express";
+import http from "http";
 import { matchRouter } from "./routes/matches.ts";
+import { attachWebSocketServer } from "./ws/server.ts";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 8000;
+
 const app = express();
+const server = http.createServer(app);
+
 app.use(express.json());
 
 app.use("/matches", matchRouter);
+
+const { broadcastMatchCreated } = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
 
 app.get("/", (_: Request, res: Response) => {
   res.send("Welcome to the Sportz Server!");
 });
 
-app.listen(8000, () => console.log("Express Server listening on PORT 8000!"));
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`WebSocket Server is running on ws://localhost:${PORT}/ws`);
+});
